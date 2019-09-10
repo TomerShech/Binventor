64fababeb82db58d855c02ccaecf275d51cf324d
TomerShech <tomers1504@gmail.com>
2019-09-10 06:42:49 +0300

    added README

---
+++ README.md
@@ -0,0 +1,3 @@
+# QPastes
+This is a web app similar to pastebin.com for sharing code and text snippets.
+Made for my Computer Science class.
--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/p/", methods=["POST"])
-def submit():
+@app.route("/p/<randid>", methods=["POST"])
+def submit(randid):
     if request.method == "POST":
         paste_body = request.form["paste_body"]
         paste_name = "Nameless" if request.form["paste_name"] == "" else request.form["paste_name"]
         expiration_time = request.form["expiration"]
-        return render_template("paste.html", title=paste_name, paste_body=paste_body)
+        return render_template("paste.html", title=paste_name, paste_body=paste_body, randid=randid)
 
 
 if __name__ == "__main__":
