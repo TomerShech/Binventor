2ed36739f6d252573cd3e0061c0d992f25f58a7d
TomerShech <tomers1504@gmail.com>
2019-09-16 22:59:18 +0300

    added auto scheduled deletion

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 from datetime import datetime
 from uuid import uuid4
 
@@ -18,7 +19,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 DB = SQLAlchemy(app)
 
-
 class BinventorDB(DB.Model):
     __tablename__ = "pastes"
 
@@ -28,7 +28,7 @@ class BinventorDB(DB.Model):
     pbody = DB.Column(DB.String(35000), nullable=False)
     # paste expiration time (in minutes)
     pexr = DB.Column(DB.Integer, nullable=False)
-    random_uuid = DB.Column(DB.String(12), nullable=False)
+    random_uuid = DB.Column(DB.String(8), nullable=False)
     created_at = DB.Column(DB.DateTime, nullable=False, default=datetime.utcnow)
 
     def __init__(self, pname, pbody, pexr, random_uuid, created_at=created_at):
@@ -37,11 +37,17 @@ class BinventorDB(DB.Model):
         self.pexr = pexr
         self.random_uuid = random_uuid
 
+def generate_id(length):
+    return str(uuid4().hex)[:length]
+
+def delete_expired():
+    DB.session.query(BinventorDB).filter((BinventorDB.created_at + func.make_interval(0, 0, 0, 0, 0, BinventorDB.pexr)) < datetime.utcnow()).\
+    delete(synchronize_session=False)
+    print('hey')
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
 
 @app.route("/submit", methods=["GET", "POST"])
 def submit():
@@ -49,26 +55,27 @@ def submit():
         paste_body = request.form["paste_body"]
         paste_name = "nameless" if not request.form["paste_name"].strip() else request.form["paste_name"]
         expiration_time = request.form["expiration"]
-        random_uuid = str(uuid4().hex)[:12]
+        random_uuid = generate_id(8)
 
         if not DB.session.query(BinventorDB).filter(BinventorDB.random_uuid == random_uuid).count():
+            delete_expired()
             data = BinventorDB(paste_name, paste_body, expiration_time, random_uuid)
             DB.session.add(data)
             DB.session.commit()
-            return redirect(url_for("paste", puuid=random_uuid, pbody=paste_body))
+            return redirect(url_for("paste", puuid=random_uuid))
         else:
             return redirect(url_for("index"))
 
 
 @app.route("/<puuid>", methods=["GET", "POST"])
 def paste(puuid):
-    return render_template("paste.html", pbody=BinventorDB.pbody)
+    delete_expired()
+    c = BinventorDB.query.filter_by(random_uuid=puuid).first_or_404()
+    return render_template("paste.html", pbody=c.pbody, title=c.pname, is_paste=True)
 
-
-# @app.route('/user/<username>')
-# def show_user(username):
-#     user = User.query.filter_by(username=username).first_or_404()
-#     return render_template('show_user.html', user=user)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", title="404 Not Found"), 404
 
 if __name__ == "__main__":
     app.debug = True
